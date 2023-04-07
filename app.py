4471b3048e1be570b3534e5f601ca6ab5342ccf4
Your Name <you@example.com>
2023-04-06 17:46:45 -0700

    updated version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, abort
 import pymysql
 from flask_cors import CORS
 
@@ -12,6 +12,24 @@ conn = pymysql.connect(
 		cursorclass=pymysql.cursors.DictCursor
         )
 cur = conn.cursor()
+@app.route('/')
+@app.route('/list-example')
+def index():
+    cur.execute("SELECT * FROM users")
+    result = cur.fetchall()
+    return 'Done!'
+
+@app.route('/register', methods = [ 'GET','POST'])
+def register():
+    Username = request.args.get('username')
+    email = request.args.get('email')
+    password = request.args.get('password')
+    cur.execute('INSERT INTO users VALUES (NULL, % s, % s, % s)',(Username,email,password,))
+    conn.commit()
+    return 'DONE'
+
+
+
 
 
 
