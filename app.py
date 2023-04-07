e55e7e9f47e3d70e477952de2652bfd9a259fd15
Your Name <you@example.com>
2023-04-06 23:23:26 -0700

    user registration finished

--- app.py
+++ app.py
@@ -13,19 +13,21 @@ conn = pymysql.connect(
         )
 cur = conn.cursor()
 @app.route('/')
-@app.route('/list-example')
-def index():
-    cur.execute("SELECT * FROM users")
-    result = cur.fetchall()
-    return 'Done!'
-
 @app.route('/register', methods = [ 'GET','POST'])
 def register():
     Username = request.args.get('username')
     email = request.args.get('email')
     password = request.args.get('password')
-    cur.execute('INSERT INTO users VALUES (NULL, % s, % s, % s)',(Username,email,password,))
+    cur.execute('SELECT * FROM users WHERE Username = % s', (Username,))
+    account = cur.fetchone()
     conn.commit()
+    if account is not None:
+        return 'account exist!'
+        
+    else:
+        cur.execute('INSERT INTO users VALUES (NULL, % s, % s, % s)',(Username,email,password,))
+        conn.commit()
+        return 'account created'
     return 'DONE'
 
 
